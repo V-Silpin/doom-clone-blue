cfa431a26f5f338e9ad84ee28196e793c58c1827
V-Silpin <varun.mohanta323@gmail.com>
2026-04-22 21:27:27 +0530

    Added Game Config

---
+++ level/const.py
@@ -0,0 +1,34 @@
+import math
+
+# game settings
+RES = WIDTH, HEIGHT = 1200, 800
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+FPS = 60
+
+PLAYER_POS = 1.5, 1.5
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002
+
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 30
+
+SCREEN_DIST = HALF_WIDTH / math.tan(HALF_FOV)
+SCALE = WIDTH // NUM_RAYS
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+DARKGRAY = (40, 40, 40)
+PURPLE = (120, 0, 120)
+CYAN = (0, 255, 255)
+
+# map settings
+TILE = 100
